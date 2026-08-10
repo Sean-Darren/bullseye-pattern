03064b5aea9a7cd0abf43524b8ff69581a484ac5
Sean Darren Timothy <seandarren00@gmail.com>
2026-08-10 10:25:01 +0800

    add initial configuration for trained models

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+BASE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_DIRECTORY = os.path.join(BASE_DIRECTORY, "models")
+MODEL_PATH = os.path.join()
+
+DEFAUL_CONFIDENCE0_TRESHOLD = 0.25
+IMAGE_SIZE = (640, 640)
+
+TEMP_RAW_CHART = os.path.join(BASE_DIRECTORY, "temp_raw.png")
+TEMP_DETECTED_CHART = os.path.join(BASE_DIRECTORY, "temp_detected.png")
+
+#data default 
+DEFAULT_TICKER = "0700.HK"
+DEFAULT_PERIOD = "6mo"
+DEFAULT_INTERVAL = "1d"
